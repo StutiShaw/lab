26774e6ec6049b826e7df5b3a167244d2d059e2a
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-24 12:58:27 +0530

    Update Cjava.java

--- Cjava.java
+++ Cjava.java
@@ -2,9 +2,13 @@ class Cjava{
   public static void main(String args[]){
           int a=10;
           int b=20;
+    //to print addition of numbers 
           System.out.println(a+b);
+    //to print subtraction of numbers
           System.out.println(b-a);
+    //to print multiplication of numbers
           System.out.println(a*b);
+    //to print division of numbers
           System.out.println(b/a);
 }
 }
