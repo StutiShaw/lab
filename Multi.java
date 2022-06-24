3d382e84635152efb1d2fcc99a9a6123c266b98f
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-24 11:34:46 +0530

    Update Multi.java

--- Multi.java
+++ Multi.java
@@ -2,6 +2,7 @@ class Multi{
   public static void main(String args[]){
           int a=5;
           int b=20;
+    //to print the multiplication value
           System.out.println(a*b);
           
 }
