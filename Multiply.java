6b4316db44c98342c1e5678557d766ba43aeb89f
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-24 11:29:25 +0530

    Update Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,10 +1,12 @@
 class Multiply
 {
   public static void main(String args[]){
+    // f is to ensure that numbers are float data type
           float a = 3.8f; 
           float b = 2.5f;
-         
-           float multiply = a*b; 
+         //to store the multiplied value
+           float multiply = a*b;
+    //to print the statement
           System.out.println("The multiplication is: "+ multiply); 
          
 }
