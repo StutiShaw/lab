7f9c8249c0172f4b51a1267491dd84119262f0d3
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-24 11:07:57 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+class Multiply
+{
+  public static void main(String args[]){
+          float a = 3.8f; 
+          float b = 2.5f;
+         
+           float multiply = a*b; 
+          System.out.println("The multiplication is: "+ multiply); 
+         
+}
+}
