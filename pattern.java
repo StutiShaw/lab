9fb405a85b32bfa2b18a1ff5bffb435d806776a0
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-30 18:01:34 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -2,16 +2,16 @@ public class pattern
 {    
 public static void main(String args[])   
 {     
-int i, j, n=5;         
+int i, j, n=5;         // declaring variable
 for (i=0; i<n; i++)   
 {        
 for (j=n-i; j>1; j--)   
 {    
-System.out.print(" ");   
+System.out.print(" ");   // to print space
 }     
 for (j=0; j<=i; j++ )   
 {         
-System.out.print("* ");   
+System.out.print("* ");   // to print star
 }     
 System.out.println();   
 }   
