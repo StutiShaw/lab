205fb8cc3b4183dd5a06a58a274a784702aa8a69
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-30 18:00:24 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -3,15 +3,15 @@ public class Prime
 {    
  public static void main(String args[])
  {    
-  int i,a=0,f=0;   
-    Scanner sc=new Scanner(System.in);
+  int i,a=0,f=0;   // declaring variable
+    Scanner sc=new Scanner(System.in);  // creating scanner
 	System.out.print("enter the number :");
 	int n=sc.nextInt();  
       
   a=n/2;      
-  if(n==0||n==1)
+  if(n==0||n==1)  // if statement
   {  
-   System.out.println(n+" is not prime number");      
+   System.out.println(n+" is not prime number");    //  to print statement  
   }
   else
   {  
@@ -19,14 +19,14 @@ public class Prime
    {      
     if(n%i==0)
 	{      
-     System.out.println(n+" is not prime number");      
+     System.out.println(n+" is not prime number");     // to print statement  
      f=1;      
      break;      
     }      
    }      
    if(f==0) 
    {
-   System.out.println(n+" is prime number"); }  
+   System.out.println(n+" is prime number"); }  //to print statement  
   }  
 }    
 }
