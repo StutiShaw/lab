d653c2e39ad068ec19f54c628e3c723230f64786
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-30 17:55:26 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -2,18 +2,18 @@ public class Armstrong {
 
     public static void main(String[] args) {
 
-        int n = 371, number, temp, t = 0;
+        int n = 371, number, temp, t = 0; //declaring variable
 
         number = n;
         while (n!= 0)
         {
-            temp = n % 10;
+            temp = n % 10;  // logic
             t = t + temp*temp*temp;
             n =n / 10;
         }
 
         if(t == number)
-            System.out.println("The number is an Armstrong number :"+number);
+            System.out.println("The number is an Armstrong number :"+number);  // to print statement
         else
             System.out.println("The number is not an Armstrong number :"+number);
     }
