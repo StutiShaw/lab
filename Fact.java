7fa7c83a7b2ccff666198a5c264e01b51f352941
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-30 17:56:43 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -3,17 +3,17 @@ class Fact
 {
   public static void main(String args[])
   {
-    int n, c, f = 1;
+    int n, c, f = 1;  //declaring variable
 	System.out.println("Enter an integer to calculate its factorial");
-    Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);  // create scanner
     n = in.nextInt();
     if (n < 0)
       System.out.println("Number should be non-negative.");
     else
     {
-      for (c = 1; c <= n; c++)
+      for (c = 1; c <= n; c++)   // for loop
         f = f*c;
-      System.out.println("Factorial of "+n+" is = "+f);
+      System.out.println("Factorial of "+n+" is = "+f);// to print statement
     }
   }
 }
