b8a3fe7cb5db36342a4cd001590572368595c9c7
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-07-26 10:35:30 +0530

    Update DupElement.java

--- DupElement.java
+++ DupElement.java
@@ -1,11 +1,11 @@
 package array;
 import java.util.Arrays; 
-public class DupElement {
+public class DupElement {  /// class
 	public static void main(String[] args) 
     {
-        int[] my_array = {3, 2, 7, 7, 6, 6, 5, 2};
+        int[] my_array = {3, 2, 7, 7, 6, 6, 5, 2};   //  initializing array
  
-        for (int i = 0; i < my_array.length-1; i++)
+        for (int i = 0; i < my_array.length-1; i++) //logic for print
         {
             for (int j = i+1; j < my_array.length; j++)
             {
