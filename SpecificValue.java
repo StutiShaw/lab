40acd89ac0ffb832c9225949b463a7586459df6a
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-07-26 10:40:38 +0530

    Update SpecificValue.java

--- SpecificValue.java
+++ SpecificValue.java
@@ -3,7 +3,7 @@ package array;
 public class SpecificValue {  // class 
 	public static boolean contains(int[] arr, int item) {  // using for if loop to check element
 	      for (int n : arr) {
-	         if (item == n) {
+	         if (item == n) {   // condition
 	            return true;
 	         }
 	      }
@@ -14,7 +14,7 @@ public class SpecificValue {  // class
 	            1789, 2035, 1899, 1456, 2013, 
 	            1458, 2458, 1254, 1472, 2365, 
 	            1456, 2265, 1457, 2456};
-	      System.out.println(contains(my_array1, 2013));
-	      System.out.println(contains(my_array1, 2015));
+	      System.out.println(contains(my_array1, 2013));  // array initialization output
+	      System.out.println(contains(my_array1, 2015));  // array initialization output
 	   }
 }
